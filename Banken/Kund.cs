342f6250bda6d1cfaecc3e60e2c5aa8105a03274
Marcus Filipsson <macke2002@outlook.com>
2020-09-10 10:32:55 +0200

    Add project files.

---
+++ Banken/Kund.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banken
+{
+    class Kund
+    {
+        public String Namn; 
+        public int Saldo
+    }
+}
